ee4f55f0b1508e5c120aaee8801b24e358cd128b
Sujith-1608 <ssujithreddi@gmail.com>
2025-01-07 13:46:27 +0530

    Prepared for deployment on Render

--- games_project/settings.py
+++ games_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m$ob03-3*2x#it!8z8=uifs26ufg4$v9b9=t!(bm!4y(c+#m#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
